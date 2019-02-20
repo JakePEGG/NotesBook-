ce1df8cafd90bfc9af3f7b9eb596c6b4355ea114
Jake Atkin <jake.a.atkin@gmail.com>
2019-02-20 14:40:04 +0000

    Second test created for HTML note list

--- tests/note-list-view-test.js
+++ tests/note-list-view-test.js
@@ -1,11 +1,14 @@
-function isANoteListView(note) {
+function isANoteListView() {
   let constructor_name = "NoteListView"
   let noteListView = new NoteListView();
   assert.isTrue(noteListView.__proto__.constructor.name === constructor_name)
 };
   isANoteListView();
-//
-// function
-//
-// <ul><li><div>Favourite food: pesto</div></li>
-// <li><div>Favourite drink: seltzer</div></li></ul>
+
+
+function displayNoteLists() {
+  let noteList = new NoteList();
+  let noteListView = new NoteListView(notelist);
+  assert.isTrue(noteListView.getHTML() === '<ul><li><div>Favourite food: Chicken</div></li><li><div>Favourite drink: H20</div></li></ul>')
+};
+displayNoteLists();
