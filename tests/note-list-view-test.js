f49503a9aa8931eb85359d519c10fb06d7e26257
Jake Atkin <jake.a.atkin@gmail.com>
2019-02-20 14:21:11 +0000

    Added initial test for notelistview

---
+++ tests/note-list-view-test.js
@@ -0,0 +1,10 @@
+function isANoteListView(note) {
+  let constructor_name = "NoteListView"
+  assert.isANoteListView(isANoteListView.__proto__.constructor_name === constructor_name)
+};
+  isANoteListView();
+// 
+// function
+//
+// <ul><li><div>Favourite food: pesto</div></li>
+// <li><div>Favourite drink: seltzer</div></li></ul>
