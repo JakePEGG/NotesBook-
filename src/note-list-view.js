410ec6e9f721f99414ca5de4334c48c4c4db621b
Jake Atkin <jake.a.atkin@gmail.com>
2019-02-20 14:56:33 +0000

    Passed both tests

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -3,5 +3,13 @@
     this.notelist = notelist;
     };
 
+  NoteListView.prototype.getHTML = function() {
+    let html = this.notelist.list.map(note => {
+      return "<li><div>" + note.text + "</div></li>"
+    });
+    return "<ul>" + html.join("") + "</ul>"
+  }
     exports.NoteListView = NoteListView;
+
+
   })(this);
--- tests/note-list-view-test.js
+++ tests/note-list-view-test.js
@@ -8,7 +8,11 @@ function isANoteListView() {
 
 function displayNoteLists() {
   let noteList = new NoteList();
-  let noteListView = new NoteListView(notelist);
+  let noteListView = new NoteListView(noteList);
+  let note = new Note("Favourite food: Chicken")
+  let note2 = new Note("Favourite drink: H20")
+  noteList.addToList(note)
+  noteList.addToList(note2)
   assert.isTrue(noteListView.getHTML() === '<ul><li><div>Favourite food: Chicken</div></li><li><div>Favourite drink: H20</div></li></ul>')
 };
 displayNoteLists();
