006eae745e1e6c83d227ea2681c1688db66fbe91
Jake Atkin <jake.a.atkin@gmail.com>
2019-02-22 14:26:32 +0000

    Passed test for single note view

--- src/single-note-view.js
+++ src/single-note-view.js
@@ -1,6 +1,16 @@
 (function (exports) {
 
-  function singlenoteview() {
+  function SingleNoteView(note) {
     this.note = note;
-  }
-})
+  };
+  SingleNoteView.prototype.note_model = function(note){
+    var array = []
+    array.push(this.note.text);
+    array.unshift('<div>');
+    array.push('</div>');
+    return array.join("");
+  };
+
+  exports.SingleNoteView = SingleNoteView;
+
+})(this);
--- tests/single-note-view-test.js
+++ tests/single-note-view-test.js
@@ -1,7 +1,7 @@
 function testIndividualNoteinHTML() {
   var note = new Note("Favourite drink: seltzer")
   var singlenoteview = new SingleNoteView(note);
-  assert.isTrue(single_note.note_model() == "<div>Favourite drink: seltzer</div>")
+  assert.isTrue(singlenoteview.note_model() == "<div>Favourite drink: seltzer</div>")
 }
 
 testIndividualNoteinHTML()
